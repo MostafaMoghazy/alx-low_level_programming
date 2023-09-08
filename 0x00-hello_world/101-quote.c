a5f819f336f27e1877c7434589204e0fd3b63a83
MostafaMoghazy <1919057@stemegypt.edu.eg>
2023-09-07 18:58:28 -0700

    Advanced Tasks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
 
 int main(void)
 {
-	char arr_ch = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char arr_ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	write(1, arr_ch, sizeof(arr_ch) / sizeof(char));
 	return (1);
