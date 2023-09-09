94ce7279a9f66e965dcd247199c0592bddf82fb2
MostafaMoghazy <1919057@stemegypt.edu.eg>
2023-09-08 20:03:51 -0700

    m

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,15 +17,18 @@ int main(void)
 	{
 		for (j = i; j <= 99; j++)
 		{
-			putchar((i / 10) + '0');
-			putchar((i % 10) + '0');
-			putchar(' ');
-			putchar((j / 10) + '0');
-			putchar((j % 10) + '0');
-			if (i == 98 && j == 99)
-				continue;
-			putchar(',');
-			putchar(' ');
+			if (i != j)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				if (i == 98 && j == 99)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
