74ca8b55c8b9eb616691c741e1282da7bfc61a5e
MostafaMoghazy <1919057@stemegypt.edu.eg>
2023-09-08 17:14:52 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program which prints from a to z then endl
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char Char;
+	
+	for (Char = 'a'; Char <= 'z'; Char++)
+	{
+		putchar(Char);
+	}
+
+	for (Char = 'A'; Char <= 'Z'; Char++)
+	{
+		putchar(Char);
+	}
+	putchar('\n');
+	return (0);
+}
