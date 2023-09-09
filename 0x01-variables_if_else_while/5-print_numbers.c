c2c93ca10e9c315db7f07f5d273057e708ece6b9
MostafaMoghazy <1919057@stemegypt.edu.eg>
2023-09-08 17:50:36 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program which prints from 0 to 9 endl
+ *
+ * Retutn: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+	return (0);
+}
