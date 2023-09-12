e0a8e8ed31f8f0e6811701f6825258040cb144b6
MostafaMoghazy <1919057@stemegypt.edu.eg>
2023-09-13 01:19:58 +0300

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a numebr
+ *
+ * @c: gives the input to the function
+ *
+ * Return: returns 1 if + , 0 if 0 , -1 if -
+ */
+
+int print_sign(int c)
+{
+	if (c > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (c == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1)
+	}
+}
