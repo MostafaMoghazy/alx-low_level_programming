a3f633465158f864277e8c0022b1f7947d85f728
MostafaMoghazy <1919057@stemegypt.edu.eg>
2023-09-13 04:50:56 +0300

    ,

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -13,7 +13,7 @@ int main(void)
 	int sum = 0, i;
 
 	for (i = 0; i < 1024; i++)
-		if ((i % 3 == 0) && (i % 5 == 0))
+		if ((i % 3 == 0) || (i % 5 == 0))
 			sum += i;
 	printf("%d\n", sum);
 	return (0);
