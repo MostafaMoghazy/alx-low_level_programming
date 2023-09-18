11358adfd5850cea03121a8722f635022a9197b6
MostafaMoghazy <1919057@stemegypt.edu.eg>
2023-09-18 23:36:02 +0300

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset the value the pointer points to 98
+ */
+
+void reset_to_98(int *n)
+{
+    *n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define Main_H
+
+#include <stdio.h>
+
+void reset_to_98(int *n);
+
+#endif
