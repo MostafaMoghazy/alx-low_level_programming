fc2384c823dd2e8481cb820576b7a97bdd5f5e95
MostafaMoghazy <1919057@stemegypt.edu.eg>
2023-09-19 01:25:48 +0300

    I can only go one way. I've not got a reverse gear

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string using pointers in reverse order
+ *
+ * @s: holds the location of the first charachter of a string
+ */
+
+void print_rev(char *s)
+{
+	char sinit = *s;
+	while (*s != '\0')
+	{
+		s++;
+	}
+	s--;
+	while (*s != sinit)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
