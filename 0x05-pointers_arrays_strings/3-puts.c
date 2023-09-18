f89fb13c8a92c3794f0506114cdd0bfd994afcd5
MostafaMoghazy <1919057@stemegypt.edu.eg>
2023-09-19 01:10:31 +0300

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string using pointers
+ *
+ * @str: holds the location of the first charachter of a string
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+}
