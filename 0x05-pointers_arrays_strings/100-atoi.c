138b530a0c462d9f75212b13b3f78082521e920e
MostafaMoghazy <1919057@stemegypt.edu.eg>
2023-09-19 04:36:06 +0300

    1st

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,20 +13,18 @@ int _atoi(char *s)
 	int result = 0;
 	int sign = 1;
 
-	while (*s == ' ' || *s == '\t' || *s == '\n')
-	{
-		s++;
-	}
+	do {
+		if (*s == '-')
+		{
+			sign *= -1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			result = (result * 10) + (*s - '0');
+		}
+		else if (result > 0)
+			break;
+	} while (s++);
 
-	if (*s == '-')
-	{
-		sign *= -1;
-		s++;
-	}
-	while (*s >= 0 && *s <= 9)
-	{
-		result = result * 10 + (*s + '0');
-		s++;
-	}
 	return (result * sign);
 }
