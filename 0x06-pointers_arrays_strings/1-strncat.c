99daf6a48d15f73e2fb474eff7f6091c7952d551
MostafaMoghazy <1919057@stemegypt.edu.eg>
2023-09-20 22:50:34 +0300

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcat - This function appends the src string to the dest string
+ *
+ * @dest: holds the location of the first sring
+ * @src: holds the location of the secin sring
+ * @n: determine the max concatination chars from the second string
+ *
+ * Return: returns the value of the string after cat
+ */
+
+char char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i]; i++)
+		;
+	for (j = 0; src[n]; j++)
+		dest[i++] = src[j];
+
+	return (dest);
+}
