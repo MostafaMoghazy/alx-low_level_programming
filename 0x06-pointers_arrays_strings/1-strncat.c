f477e225a7b646bfa70c6527a4a6b6e690b62d85
MostafaMoghazy <1919057@stemegypt.edu.eg>
2023-09-20 23:21:49 +0300

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,7 +16,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (i = 0; dest[i]; i++)
 		;
-	for (j = 0; src[n]; j++)
+	for (j = 0; j < n && src[j] != '\0'; j++)
 		dest[i++] = src[j];
 
 	return (dest);
