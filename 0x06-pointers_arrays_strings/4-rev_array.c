6663151802de2a76f6231e247ccb7977e7e0d93a
MostafaMoghazy <1919057@stemegypt.edu.eg>
2023-09-22 01:58:59 +0300

    am a kind of paranoid in reverse. I suspect people of plotting to make me happy

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - This function reverses array elements
+ *
+ * @a: holds the location of  the array first element
+ * @n: determine the size of the array
+ */
+
+void reverse_array(int *a, int n)
+{
+    int i, j, swap;
+
+    for (i = 0, j = n - 1; i < j; i++, j--)
+    {
+        swap = a[j];
+        a[j] = a[i];
+        a[i] = swap;
+    }
+}
