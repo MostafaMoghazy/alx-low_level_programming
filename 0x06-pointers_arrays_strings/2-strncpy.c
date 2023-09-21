89bff471f7692b2cb9471ea3cfbd7ec7953fff21
MostafaMoghazy <1919057@stemegypt.edu.eg>
2023-09-22 00:52:17 +0300

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -17,7 +17,10 @@ char *_strncpy(char *dest, char *src, int n)
 	for (j = 0; j < n && src[j] != '\0'; j++)
 		dest[j] = src[j];
 	while (j < n)
+	{
 		dest[j] = '\0';
+		j++
+	}
 
 	return (dest);
 }
