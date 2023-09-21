aac9ba9fc39690dbef7b620cd8b2961f00917f79
MostafaMoghazy <1919057@stemegypt.edu.eg>
2023-09-22 01:03:46 +0300

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares between two strings
+ *
+ * @s1: holds the location of the first string
+ * @s2: holds the location of the second string
+ *
+ * Return: 0 if true and otherwise when false
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+    int i, check;
+
+    check = 0;
+    for (i = 0; s1[i] != '\0'; i++)
+    {
+        if (s1[i] != s2[i])
+            check = i;
+    }
+    return (check);
+}
