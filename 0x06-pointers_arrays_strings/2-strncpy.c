b7948c2329cbc666788239f906084613d0ee0e86
MostafaMoghazy <1919057@stemegypt.edu.eg>
2023-09-22 00:31:21 +0300

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy - This function coppies the src string to the dest string
+ *
+ * @dest: holds the location of the first sring
+ * @src: holds the location of the secin sring
+ * @n: determine the max concatination chars from the second string
+ *
+ * Return: returns the value of the string after cp
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	for (j = 0; j < n && src[j] != '\0'; j++)
+		dest[j] = src[j];
+	if (j < n)
+		dest[j] = '\0';
+
+	return (dest);
+}
