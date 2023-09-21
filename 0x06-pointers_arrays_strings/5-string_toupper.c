2bf6f0b1a9e54cabdaaf5f4de50bfa222bfcf925
MostafaMoghazy <1919057@stemegypt.edu.eg>
2023-09-22 02:10:38 +0300

    Always to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - This function coppies the src string to the dest string
+ *
+ * @string: holds the location of the string's first element
+ *
+ * Return: returns a pointer of the string after converting to upper
+ */
+
+char *string_toupper(char *string)
+{
+	int i;
+
+	for (i = 0; string[i] != '\0'; i++)
+	{
+		if (string[i] >= 97 && string[i] <= 122)
+		{
+			string[i] = string[i] - 32;
+		}
+	}
+	return (string);
+}
