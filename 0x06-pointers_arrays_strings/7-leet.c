30cc50b7762211a490f47b9d23f56958bc59293b
MostafaMoghazy <1919057@stemegypt.edu.eg>
2023-09-22 03:59:26 +0300

    hello, world

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,29 +9,21 @@
  */
 char *leet(char *str)
 {
-	char leetMap[128];
+	unsigned int i;
+	char *encoded = str;
+	int Quema[] = {4, 3, 0, 7, 1};
+	char K[] = {'A', 'E', 'O', 'T', 'L'};
 
-	memset(leetMap, 0, sizeof(leetMap));
-
-	leetMap['a'] = leetMap['A'] = '4';
-	leetMap['e'] = leetMap['E'] = '3';
-	leetMap['o'] = leetMap['O'] = '0';
-	leetMap['t'] = leetMap['T'] = '7';
-	leetMap['l'] = leetMap['L'] = '1';
-
-	size_t len = strlen(str);
-	char *encoded = (char *)malloc((len + 1) * sizeof(char));
-	size_t i = 0;
-
-	for (; i < len; i++)
+	while (*str)
 	{
-		char currentChar = str[i];
-		char replacement = leetMap[currentChar];
-
-		encoded[i] = (replacement != '\0') ? replacement : currentChar;
+		for (i = 0; i < sizeof(K) / sizeof(char); i++)
+		{
+			if (*str == K[i] || *str == K[i] + 32)
+			{
+				*str = 48 + Quema[i];
+			}
+		}
+		str++;
 	}
-
-	encoded[i] = '\0';
-
 	return (encoded);
 }
