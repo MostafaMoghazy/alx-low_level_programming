3afc9ff59d0dab53cae4fe72603e6871c05900ed
MostafaMoghazy <1919057@stemegypt.edu.eg>
2023-09-22 04:57:23 +0300

    hot fix

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,26 +1,19 @@
 #include <stdio.h>
+
 /**
- * main - magic function
+ * main - entry point
  * Return: 0 success
  */
 int main(void)
 {
-	int n;
+	int num;
 	int array[5];
 	int *ptr;
 
 	array[2] = 1024;
-	ptr = &n;
-	/*
-	 * write your line of code here...
-	 * Remember:
-	 * - you are not allowed to use array
-	 * - you are not allowed to modify ptr
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
-	 */
+	ptr = &num;
+
 	*(ptr + 5) = 98;
-	/* ...so that this prints 98\n */
-	printf("array[2] = %d\n", array[2]);
+	printf("a[2] = %d\n", array[2]);
 	return (0);
 }
