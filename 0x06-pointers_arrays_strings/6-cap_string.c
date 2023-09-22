68a6c02007e1b84500751123b353d1542a4dabd1
MostafaMoghazy <1919057@stemegypt.edu.eg>
2023-09-22 03:18:52 +0300

    test

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,27 +1,56 @@
 #include "main.h"
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * isSeparator - to check if a character is a word separator
+ * @c: holds a value
+ * Return: 0 or 1
+ */
+int isSeparator(char c)
+{
+	char separators[] = " \t\n,;.!?\"(){}";
+	int i;
+
+	for (i = 0; separators[i] != '\0'; i++)
+	{
+		if (c == separators[i])
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
 
 /**
  * cap_string - This function capetalize the string
  *
- * @string: holds the location of the string's first element
+ * @str: holds the location of the string's first element
  *
  * Return: returns a pointer of the string after capetalizing
  */
 
-char *cap_string(char *string)
+char *cap_string(char *str)
 {
-	int i;
+	int len = strlen(str);
+	int capitalizeNext = 1, i;
 
-	for (i = 0; string[i] != '\0'; i++)
+	for (i = 0; i < len; i++)
 	{
-		if (string[i] == 32)
+		if (isSeparator(str[i]))
 		{
-			i++;
-			if (string[i] >= 97 && string[i] <= 122)
-			{
-				string[i] = string[i] - 32;
-			}
+			capitalizeNext = 1;
+		}
+		else if (capitalizeNext)
+		{
+			str[i] = toupper(str[i]);
+			capitalizeNext = 0;
+		}
+		else
+		{
+			str[i] = tolower(str[i]);
 		}
 	}
-	return (string);
+
+	return (str);
 }
