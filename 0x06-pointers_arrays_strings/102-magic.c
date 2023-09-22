ff2d982d0959cef967be30252022b92f7caaf488
MostafaMoghazy <1919057@stemegypt.edu.eg>
2023-09-22 05:00:01 +0300

    hot fix

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 
-/**
- * main - entry point
- * Return: 0 success
- */
 int main(void)
 {
-	int num;
-	int array[5];
+	int n;
+	int arr[5];
 	int *ptr;
 
-	array[2] = 1024;
-	ptr = &num;
-
+	arr[2] = 1024;
+	ptr = &n;
+	/*
+	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
+	 */
 	*(ptr + 5) = 98;
-	printf("a[2] = %d\n", array[2]);
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", arr[2]);
 	return (0);
 }
