5f6934d703bd527ef68382ca206748df4b00bccf
MostafaMoghazy <1919057@stemegypt.edu.eg>
2023-10-03 03:31:18 +0300

    hot fix

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,42 +9,42 @@
  */
 int main(int argc, char **argv)
 {
-    int cents[] = {25, 10, 5, 2};
-    unsigned int x;
-    char *ptr;
-    int tot, count;
+	int cents[] = {25, 10, 5, 2};
+	unsigned int x;
+	char *ptr;
+	int tot, count;
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return (1);
-    }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    tot = strtol(argv[1], &ptr, 10);
-    count = 0;
+	tot = strtol(argv[1], &ptr, 10);
+	count = 0;
 
-    if (!*ptr)
-    {
-        while (tot > 1)
-        {
-            for (x = 0; x < sizeof(cents[x]); x++)
-            {
-                if (tot >= cents[x])
-                {
-                    count += tot / cents[x];
-                    tot = tot % cents[x];
-                }
-            }
-        }
-        if (tot == 1)
-            count++;
-    }
-    else
-    {
-        printf("Error\n");
-        return (1);
-    }
+	if (!*ptr)
+	{
+		while (tot > 1)
+		{
+			for (x = 0; x < sizeof(cents[x]); x++)
+			{
+				if (tot >= cents[x])
+				{
+					count += tot / cents[x];
+					tot = tot % cents[x];
+				}
+			}
+		}
+		if (tot == 1)
+			count++;
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    printf("%d\n", count);
-    return (0);
+	printf("%d\n", count);
+	return (0);
 }
